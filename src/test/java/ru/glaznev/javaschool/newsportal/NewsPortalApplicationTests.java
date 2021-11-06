e3dc47733b469304fe7014622fcdeaaf7bfc0a71
Vsevolod Glaznev <russian.cooler@gmail.com>
2021-11-06 16:04:52 +0300

    Initial commit

---
+++ src/test/java/ru/glaznev/javaschool/newsportal/NewsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glaznev.javaschool.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
